ee1113a7e1df2c44915a8c27ba20638d35e14afa
jordan.werre <jordan.r.werre@gmail.com>
2021-04-02 15:58:31 -0400

    validation fine tune

--- routes/page.js
+++ routes/page.js
@@ -1,7 +1,9 @@
-const express = require('expresss');
+const express = require('express');
 
 const pageController = require('../controllers/page');
 
 const router = express.Router();
 
 router.get('/', pageController.mainPage);
+
+module.exports = router;
